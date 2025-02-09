c92e3dde698b29f4e167725d0c20fafc16354aa1
nadaalynasr <nadaalynasr@gmail.com>
2025-02-09 00:08:59 -0500

    Fixed user input text color

--- src/app/components/ui/input.tsx
+++ src/app/components/ui/input.tsx
@@ -9,10 +9,10 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type,
     <input
       type={type}
       className={cn(
-        "flex h-10 w-full rounded-md border border-primary-text bg-primary-background px-3 py-2 text-sm font-bold ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-primary-text focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 font-aileron",
+        "flex h-10 w-full rounded-md border border-primary-text bg-primary-background px-3 py-2 text-sm font-bold ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-primary-text text-[rgb(221,161,94)] focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 font-aileron",
         className,
       )}
-      style={{ color: '#DDA15E' }}
+      style={{ color: 'rgb(254, 250, 224)' }}
       ref={ref}
       {...props}
     />
